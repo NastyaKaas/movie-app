db9589762c3163fdea914c10a4604741827fd905
Anastasiia Kachesova <98601803+NastyaKaas@users.noreply.github.com>
2022-03-02 23:02:22 +0800

    Add files via upload

---
+++ MovieApp.js
@@ -0,0 +1,111 @@
+let formContainer = document.querySelector(".form-container");
+let searchLine = document.querySelector(".search-line");
+let main = document.querySelector("main");
+let resultsContainer = document.querySelector(".results-container");
+
+// fetch movies
+let getMovies = async (inputValue) => {
+  let response = await fetch(
+    `http://www.omdbapi.com/?s=${inputValue}&apikey=535b116`
+  );
+
+  if (!response.ok) {
+    throw new Error("Something Went Wrong");
+  }
+  let moviesObj = await response.json();
+  let movies = moviesObj.Search;
+  return movies;
+};
+
+// submit
+if (formContainer) {
+  formContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let inputValue = searchLine.value;
+    resultsContainer.classList.remove("show");
+
+    // display a movie list on the main screen
+    getMovies(inputValue)
+      .then((movies) => {
+        let listOfMovies = movies
+          .map((movie) => {
+            return `<article class="article-container">
+    <img src="${movie.Poster} alt= "movie poster"> 
+    <div class="movie-details"> 
+    <h2>${movie.Title}</h2>
+    <p>Year: ${movie.Year}</p>
+    </div>
+    <div class="button-container">
+    <button id=${movie.imdbID} class="show-more-button">Show More</button>
+    <div>
+    </article>`;
+          })
+          .join("");
+        main.innerHTML = listOfMovies;
+
+        // get a movie's id by clicking on a button and redirect to another file
+
+        let showMoreButton = document.querySelectorAll(".show-more-button");
+
+        showMoreButton.forEach((button) => {
+          button.addEventListener("click", (e) => {
+            e.preventDefault();
+            let id = e.currentTarget.id;
+            showMore(id);
+          });
+        });
+      })
+      .catch((error) => console.log(error.message));
+  });
+}
+
+if (formContainer) {
+  formContainer.addEventListener("input", (e) => {
+    e.preventDefault();
+    if (searchLine.value.length >= 1) {
+      let inputValue = searchLine.value;
+      resultsContainer.classList.add("show");
+      getMovies(inputValue)
+        .then((movies) => {
+          // show the list
+          let listOfMovies = movies
+            .map((movie) => {
+              return `<ul class="list-container">
+        <a id=${movie.imdbID} class="one-movie">
+      <li><img src="${movie.Poster} alt= "movie poster" width="43px" height="49px"></li>
+      <div class="movie-description"> 
+      <li><h2>${movie.Title}</h2></li>
+      <li><p>Year: ${movie.Year}</p></li>
+      </div>
+      </a>
+      </ul>`;
+            })
+            .join("");
+          resultsContainer.innerHTML = listOfMovies;
+
+          // get a movie's id by clicking on a button and redirect to another file
+
+          let oneMovie = document.querySelectorAll(".one-movie");
+
+          oneMovie.forEach((movie) => {
+            movie.addEventListener("click", (e) => {
+              e.preventDefault();
+              console.log(e.currentTarget.id);
+              let id = e.currentTarget.id;
+              showMore(id);
+            });
+          });
+        })
+        .catch((error) => console.log(error.message));
+    } else if (searchLine.value.length < 1) {
+      resultsContainer.classList.remove("show");
+    }
+  })
+}
+
+// the function to tranfer id to another file
+
+let showMore = (id) => {
+  sessionStorage.setItem("movieID", id);
+  window.location.href = "./OneMovie.html";
+};
---
+++ OneMovie.js
@@ -0,0 +1,130 @@
+let sectionContainer = document.querySelector(".section-container");
+let preloader = document.querySelector(".preloader");
+
+// fetch a single movie
+
+let getOneMovie = async(id) => {
+    id = sessionStorage.getItem("movieID");
+
+    let response = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=535b116`);
+    
+      if (!response.ok) {
+        throw new Error("Something Went Wrong");
+      }
+
+      let movie = await response.json();
+      let poster = await movie.Poster;
+
+      document.body.innerHTML = `<section class="section-container">
+            <div class="img-container">
+            <img src="${movie.Poster} alt= "movie poster">
+            </div>
+            <h2>${movie.Title}</h2>
+            <p><span>Year:</span> ${movie.Year}</p>
+            <p><span>Actors:</span> ${movie.Actors}</p>
+            <p><span>Genre:</span> ${movie.Genre}</p>
+            <p><span>Country:</span> ${movie.Country}</p>
+            <p><span>Runtime:</span> ${movie.Runtime}</p>
+            <p><span>Director:</span> ${movie.Director}</p>
+            <p><span>Writer:</span> ${movie.Writer}</p> 
+            <p><span>Plot:</span> ${movie.Plot}</p>
+            <div class="go-back-container">
+            <a class="go-back" href="./MovieApp.html">Go Back</a>
+            </div>
+            </section>
+            `  
+            
+            return poster
+}
+
+getOneMovie()
+.then(poster => {
+   getImage(poster)
+  .then((moviePoster) => {  
+    document.body.style.cssText = `
+      bacgkround: white;
+      background: ${getAverageColor(moviePoster)};
+      background-repeat: no-repeat;
+      background-size: cover;
+       `
+       console.log(`${getAverageColor(moviePoster)}`)
+  })
+})
+.catch(error => console.log(error.message));
+
+// take a URL string and turn it into an image to use getAverageColor function
+
+let getImage = (imgURL) => {
+  return new Promise ((resolve, reject) => {
+  let img = new Image();
+  img.addEventListener("load", () => {
+    resolve(img)
+  });
+  img.addEventListener("error", reject);
+  img.src = imgURL;
+  img.crossOrigin = "Anonymous";
+  })
+}
+
+// get average color of the image
+
+let getAverageColor = (img) => {
+
+let defaultColor = {r:199, g:199, b:199},
+canvas = document.createElement("canvas"),
+context = canvas.getContext && canvas.getContext("2d"), 
+imgData, width, height, length,
+rgb = {r:0, g:0, b:0},
+count = 0;
+
+height = canvas.height = img.naturalHeight ||
+img.offsetHeight ||
+img.height;
+
+if(!context) {
+  return defaultColor;
+}
+
+width = canvas.width = img.naturalWidth ||
+img.offsetWidth ||
+img.width;
+
+context.drawImage(img, 0, 0);
+
+try {
+ imgData = context.getImageData(0, 0, canvas.width, canvas.height);  
+} catch {
+  return defaultColor;
+}
+
+
+length = imgData.data.length;   
+
+for(let i = 0; i < length; i+=4) {
+ 
+rgb.r += imgData.data[i];
+rgb.g += imgData.data[i + 1];
+rgb.b += imgData.data[i + 2];
+
+count++;
+}
+
+rgb.r = Math.floor(rgb.r/count);
+rgb.g = Math.floor(rgb.g/count);
+rgb.b = Math.floor(rgb.b/count);
+
+return `linear-gradient(rgba(${rgb.r},${rgb.g},${rgb.b},0.8),
+rgba(255, 255, 255, 0.568))`
+};
+
+
+// preloader
+
+document.addEventListener("readystatechange", () => {
+  if(document.readyState == "complete") {
+    preloader.classList.add("hide");
+  }  
+})
+
+
+
